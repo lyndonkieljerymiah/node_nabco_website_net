9de00ba41a40ece4173d0db2d7420baa08286b94
Arnold Mercado <arnold.mercado@hotmail.com>
2017-03-13 20:54:49 +0300

    update server.js

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var http = require("http");
 var express = require("express");
 var controllers = require("./controllers");
 
-var port = 2000;
-
 var app = express();
+
+app.set('port', (process.env.PORT || 2000));
 app.set("view engine", "vash");
 app.use(express.static(__dirname + "/public"));
 controllers.init(app);
 
-var server = http.createServer(app);
-server.listen(port);
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
